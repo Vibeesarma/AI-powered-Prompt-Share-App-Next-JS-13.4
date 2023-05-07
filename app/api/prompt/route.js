e73f6cb4d4a294e6e505c08d83186ae92b467168
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-05-07 08:11:13 +0100

    copy and show function done

---
+++ app/api/prompt/route.js
@@ -0,0 +1,17 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (requst) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate("creator");
+
+    return new Response(JSON.stringify(prompts), {
+      status: 201,
+    });
+  } catch (error) {
+    return new Response("Faild to fetch all prompt", {
+      status: 500,
+    });
+  }
+};
